143a6c53a921e70fbb32ae9079052cc024a648cd
manuel <manuelarr99@gmail.com>
2021-12-30 15:09:19 -0600

    Documentation added

--- src/main/java/com/ibm/academia/apirest/controller/BankController.java
+++ src/main/java/com/ibm/academia/apirest/controller/BankController.java
@@ -32,7 +32,7 @@ public class BankController {
     /**
      * EndPoint que retorna una lista de bancos y cajeros cercanos con base en los datos de ubicación
      * ingresados, se requieren dos campos obligatorios, los campos de GPS agregaran más precisión a la
-     * busqueda.
+     * búsqueda.
      *
      * @param state     cadena con el nombre del estado de la ubicación ingresada.
      * @param cp        cadena con el CP de la ubicación ingresada.
