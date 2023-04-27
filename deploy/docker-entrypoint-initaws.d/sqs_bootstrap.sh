791a91653e2d80c2f98a142ea4025918279837d5
manuelrojas19 <manuelarr99@gmail.com>
2023-04-27 16:35:24 -0600

    Modified deploy files

--- deploy/docker-entrypoint-initaws.d/sqs_bootstrap.sh
+++ deploy/docker-entrypoint-initaws.d/sqs_bootstrap.sh
@@ -12,7 +12,7 @@ AWS_REGION=eu-central-1
 
 create_queue() {
   local QUEUE_NAME_TO_CREATE=$1
-  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs create-queue --queue-name ${QUEUE_NAME_TO_CREATE} --region ${AWS_REGION} --attributes VisibilityTimeout=30
+  awslocal --endpoint-url=http://${LOCALSTACK_HOST}:4566 sqs create-queue --queue-name "${QUEUE_NAME_TO_CREATE}" --region ${AWS_REGION} --attributes VisibilityTimeout=30
 }
 
 create_queue "send-eventhub"
