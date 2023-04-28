0d1efcc7cef54132e442c87aa90ed786a19a1c97
manuelrojas19 <manuelarr99@gmail.com>
2023-04-27 18:27:44 -0600

    Modified deploy files

--- src/main/java/com/manuelr/bank/api/config/RedisCacheConfig.java
+++ src/main/java/com/manuelr/bank/api/config/RedisCacheConfig.java
@@ -1,7 +1,6 @@
 package com.manuelr.bank.api.config;
 
 import com.manuelr.bank.api.model.FindBankResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
